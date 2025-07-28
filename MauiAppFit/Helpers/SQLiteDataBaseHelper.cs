5a16cfcd7ad1fa3f2e209ef30f54670577c39f0d
PAOLA GASPARI <paola.gaspari@etec.sp.gov.br>
2025-07-28 11:04:13 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppFit/Helpers/SQLiteDataBaseHelper.cs
@@ -0,0 +1,16 @@
+﻿using MauiAppFit.Models;
+using SQLite;
+
+namespace MauiAppFit.Helpers
+{
+    public class SQLiteDataBaseHelper
+    {
+        readonly SQLiteAsyncConnection _db;
+
+        public SQLiteDataBaseHelper(string dbPath)
+        {
+            _db = new SQLiteAsyncConnection(dbPath);
+            _db.CreateTableAsync<Atividade>().Wait();
+        }
+    }
+}
