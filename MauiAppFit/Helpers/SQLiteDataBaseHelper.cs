872da5d6436199437c20cd4e6b6fc8ad62d01fa4
PAOLA GASPARI <paola.gaspari@etec.sp.gov.br>
2025-08-04 14:54:22 -0300

    avanço

--- MauiAppFit/Helpers/SQLiteDataBaseHelper.cs
+++ MauiAppFit/Helpers/SQLiteDataBaseHelper.cs
@@ -32,6 +32,27 @@ namespace MauiAppFit.Helpers
         {
             string sql = "UPDATE Atividade SET Descricao=?, Data=?, Peso=?, " +
                 "Observacoes=? WHERE Id=?";
+
+            return _db.QueryAsync<Atividade>(
+                sql,
+                model.Descricao,
+                model.Data,
+                model.Peso,
+                model.Observacoes,
+                model.Id
+            );
+        }
+
+        public Task<int> Delete(int id)
+        {
+            return _db.Table<Atividade>().DeleteAsync(i => i.Id == id);
+        }
+
+        public Task<List<Atividade>> Search(string q)
+        {
+            string sql = "SELECT * FROM Atividade WHERE Descricao LIKE '%" + q + "%'";
+
+            return _db.QueryAsync<Atividade>(sql);
         }
 
     }
